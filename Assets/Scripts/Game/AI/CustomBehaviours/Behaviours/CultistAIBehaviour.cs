bee4336909f75e8d8d3e923026309619062d8dc0
KlimkinAlexandr <aexklim@gmail.com>
2021-09-02 22:04:00 +0300

    cultist

--- Assets/Scripts/Game/AI/CustomBehaviours/Behaviours/CultistAIBehaviour.cs
+++ Assets/Scripts/Game/AI/CustomBehaviours/Behaviours/CultistAIBehaviour.cs
@@ -1,14 +1,13 @@
-using Character.Health;
+﻿using Character.Health;
 using Game.AI.CustomBehaviours.BlackboardData;
 using Game.AI.CustomBehaviours.Tasks;
 using Tools.BehaviourTree;
 
-public class ZombieFrogAIBehaviour : BehaviourTreeExecutor
+public class CultistAIBehaviour : BehaviourTreeExecutor
 {
     public MovementData MovementData;
     public MovementPointsData MovementPointsData;
     public TargetSearchData TargetSearchData;
-    public SelectAttackData SelectAttackData;
 
     private IDamageable _Damageable;
 
@@ -21,18 +20,17 @@ public class ZombieFrogAIBehaviour : BehaviourTreeExecutor
     protected override BehaviourTree BuildBehaviourTree()
     {
         var behaviourTree = new BehaviourTree();
-            var mainTree = behaviourTree.AddChild<ParallelTask>();
+        var mainTree = behaviourTree.AddChild<ParallelTask>();
             
-            mainTree.AddChild<TargetSearchTask>();
+        mainTree.AddChild<TargetSearchTask>();
             
-            var moveSelector = mainTree.AddChild<SelectorTask>();
-                moveSelector.AddChild<TargetPursuitTask>();
-                moveSelector.AddChild<PointPathSelectionTask>();
-            mainTree.AddChild<SimpleMoveToPointTask>();
-            mainTree.AddChild<SelectAttackTask>();
-            mainTree.AddChild<AttackMeleeWeaponTargetTask>();
+        var moveSelector = mainTree.AddChild<SelectorTask>();
+        moveSelector.AddChild<TargetPursuitTask>();
+        moveSelector.AddChild<PointPathSelectionTask>();
+        mainTree.AddChild<SimpleMoveToPointTask>();
+        mainTree.AddChild<AttackMeleeWeaponTargetTask>();
 
-            return behaviourTree;
+        return behaviourTree;
     }
 
     protected override Blackboard BuildBlackboard()
@@ -41,7 +39,6 @@ public class ZombieFrogAIBehaviour : BehaviourTreeExecutor
         bb.Set(MovementPointsData);
         bb.Set(TargetSearchData);
         bb.Set(MovementData);
-        bb.Set(SelectAttackData);
         return bb;
     }
     
