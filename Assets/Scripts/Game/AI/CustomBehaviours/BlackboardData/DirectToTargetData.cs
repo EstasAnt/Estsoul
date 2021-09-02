dab740ad0b99df37048c268fa63e1ee132d7b5c0
KlimkinAlexandr <aexklim@gmail.com>
2021-09-02 23:59:55 +0300

    kusaka

---
+++ Assets/Scripts/Game/AI/CustomBehaviours/BlackboardData/DirectToTargetData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Game.AI.CustomBehaviours.BlackboardData
+{
+    [Serializable]
+    public class DirectToTargetData : global::BlackboardData
+    {
+        public Transform Root;
+        public bool RotateSign;
+    }
+}
---
+++ Assets/Scripts/Game/AI/CustomBehaviours/Tasks/DirectToTargetTask.cs
@@ -0,0 +1,36 @@
+﻿using Game.AI.CustomBehaviours.BlackboardData;
+using Tools.BehaviourTree;
+using UnityEngine;
+
+namespace Game.AI.CustomBehaviours.Tasks
+{
+    public class DirectToTargetTask : BuildedTask
+    {
+        private TargetSearchData _TargetSearchData;
+        private DirectToTargetData _DirectToTargetData;
+
+        private Vector3 _StartLocalScale;
+        
+        public override void Init()
+        {
+            base.Init();
+            _TargetSearchData = Blackboard.Get<TargetSearchData>();
+            _DirectToTargetData = Blackboard.Get<DirectToTargetData>();
+            _StartLocalScale = _DirectToTargetData.Root.localScale;
+        }
+        
+        public override TaskStatus Run()
+        {
+            if (_TargetSearchData.Target == null)
+                return TaskStatus.Failure;
+            var vectorToTarget =
+                _TargetSearchData.Target.Transform.position - _DirectToTargetData.Root.position;
+            var sign = Mathf.Sign(vectorToTarget.x);
+            if (_DirectToTargetData.RotateSign)
+                sign *= -1;
+            _DirectToTargetData.Root.localScale =
+                new Vector3(_StartLocalScale.x * sign, _StartLocalScale.y, _StartLocalScale.z);
+            return TaskStatus.Success;
+        }
+    }
+}
