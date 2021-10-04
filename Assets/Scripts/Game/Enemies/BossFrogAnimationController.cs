70ef8178cb5506716a68f55c9961824b624798da
KlimkinAlexandr <aexklim@gmail.com>
2021-10-05 02:14:36 +0300

    Boss frog

---
+++ Assets/Scripts/Game/Enemies/BossFrogAnimationController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Character.Health;
+using Character.Shooting;
+using UnityDI;
+using UnityEngine;
+using System.Collections;
+
+namespace Game.Movement.Enemies
+{
+    public class BossFrogAnimationController : MonoBehaviour
+    {
+        private Animator _Animator;
+        private WeaponController _WeaponController;
+        private IDamageable _Damageable;
+        
+        private bool _Awake;
+        
+        private void Awake() {
+            ContainerHolder.Container.BuildUp(this);
+            _Animator = GetComponent<Animator>();
+            _WeaponController = GetComponentInParent<WeaponController>();
+            _Damageable = GetComponent<IDamageable>();
+            _Damageable.OnDamage += DamageableOnOnDamage;
+        }
+
+        private void Update()
+        {
+            if (_Animator == null) 
+                return;
+            _Animator.SetBool("Awake", _Awake);
+        }
+
+        private void DamageableOnOnDamage(IDamageable arg1, Damage arg2)
+        {
+            StartCoroutine(WakeUpRoutine());
+        }
+
+        private void OnDestroy()
+        {
+            _Damageable.OnDamage -= DamageableOnOnDamage;
+        }
+
+        private IEnumerator WakeUpRoutine()
+        {
+            _Awake = true;
+            yield return new WaitForSeconds(3f);
+            _Awake = false;
+        }
+        
+    }
+}
