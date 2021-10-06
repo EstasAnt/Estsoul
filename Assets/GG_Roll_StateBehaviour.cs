d23631dc4161d300fc208511855ac2b342195d43
KlimkinAlexandr <aexklim@gmail.com>
2021-10-06 04:04:43 +0300

    roll fix

--- Assets/GG_Roll_StateBehaviour.cs
+++ Assets/GG_Roll_StateBehaviour.cs
@@ -1,45 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using Game.Movement;
+using Tools.Unity;
+using UnityDI;
 using UnityEngine;
 
 public class GG_Roll_StateBehaviour : StateMachineBehaviour
 {
-    public AnimationCurve Curve;
-    public float Distance;
-    public float GravityCof;
-
-    private Animator _animator;
+    public Vector2 Force;
+    public float Acceleration;
+ 
+    private UnityEventProvider _EventProvider;
     private Rigidbody2D _rigidbody;
     private MovementController _movementController;
 
-    private Vector2 _startPosition;
-    private float _startGravityScale;
+    private float _StartGroundAcceleration;
+    private float _StartAirAcceleration;
+
+    private Coroutine _MoveRoutine;
     
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        if (_EventProvider == null)
+            _EventProvider = ContainerHolder.Container.Resolve<UnityEventProvider>();
         if (_rigidbody == null)
             _rigidbody = animator.GetComponent<Rigidbody2D>();
         if (_movementController == null)
             _movementController = animator.GetComponent<MovementController>();
-        _startPosition = _rigidbody.position;
-        _startGravityScale = _rigidbody.gravityScale;
-        _rigidbody.gravityScale = 0;
+        if(_MoveRoutine != null)
+            _EventProvider.StopCoroutine(_MoveRoutine);
+        _MoveRoutine = _EventProvider.StartCoroutine(MoveRoutine());
     }
 
-    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    private IEnumerator MoveRoutine()
     {
-        var currentPos = _rigidbody.position;
-        
-        var newXPos = _startPosition.x + Distance * Curve.Evaluate(stateInfo.normalizedTime) * _movementController.Direction;
-        //var nextYPos = currentPos.y + Physics2D.gravity.y * _rigidbody.gravityScale;
-        var gravityAdd = Physics2D.gravity * GravityCof * Time.deltaTime;
-        
-        _rigidbody.MovePosition(new Vector2(newXPos, currentPos.y) + gravityAdd);
+        yield return null;
+        _rigidbody.velocity = new Vector2(0, _rigidbody.velocity.y);
+        _rigidbody.AddForce(new Vector2(Force.x * _movementController.Direction, Force.y));
+        _StartGroundAcceleration = _movementController.WalkParameters.GroundAcceleration;
+        _StartAirAcceleration = _movementController.WalkParameters.AirAcceleration;
+        _movementController.WalkParameters.GroundAcceleration = Acceleration;
+        _movementController.WalkParameters.AirAcceleration = Acceleration; 
     }
 
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        _rigidbody.gravityScale = _startGravityScale;
+        _movementController.WalkParameters.GroundAcceleration = _StartGroundAcceleration;
+        _movementController.WalkParameters.AirAcceleration = _StartAirAcceleration;
     }
 }
