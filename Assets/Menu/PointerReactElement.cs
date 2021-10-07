3edc3146777d371e171452d67168bc15ff23ffa0
KlimkinAlexandr <aexklim@gmail.com>
2021-10-07 19:18:48 +0300

    Indicators reaction on pointer in menu

---
+++ Assets/Menu/PointerReactElement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Menu
+{
+    public class PointerReactElement : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        public List<GameObject> Elements;
+
+        private void OnEnable()
+        {
+            Elements.ForEach(_ => _.SetActive(false));
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            Elements.ForEach(_ => _.SetActive(true));
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            Elements.ForEach(_ => _.SetActive(false));
+        }
+    }
+}
