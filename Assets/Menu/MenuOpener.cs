2613e3112a9f23aba4d665f96b9a5f8bf42b5f40
Valera <karasik12345678910@gmail.com>
2021-10-10 16:49:17 +0300

    Menu object inactive when menu isnt opened

--- Assets/Menu/MenuOpener.cs
+++ Assets/Menu/MenuOpener.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityDI;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,12 +9,42 @@ public class MenuOpener : BaseMenu
     public float lastTimeScale = 1;
     public List<GameObject> backGrounds;
 
+    void Awake()
+    {
+        ContainerHolder.Container.RegisterInstance(this);
+        gameObject.SetActive(false);
+    }
+
     public override void SwitchTo(BaseMenu menu)
     {
-        enabled = false;
         menu.gameObject.SetActive(true);
         foreach(GameObject image in backGrounds) image.SetActive(true);
         lastTimeScale = Time.timeScale;
         Time.timeScale = 0;
+    }
+
+    public void Pause()
+    {
+        if (!enabled) return;
+        enabled = false;
+        gameObject.SetActive(true);
+        GetComponentInChildren<PauseMenu>(true).gameObject.SetActive(true);
+        foreach (GameObject image in backGrounds) image.SetActive(true);
+        lastTimeScale = Time.timeScale;
+        Time.timeScale = 0;
+    }
+
+    public void Continue()
+    {
+        enabled = true;
+        gameObject.SetActive(true);
+        GetComponentInChildren<PauseMenu>(true).gameObject.SetActive(false);
+        foreach (GameObject image in backGrounds) image.SetActive(false);
+        Time.timeScale = lastTimeScale;
+    }
+
+    new private void OnDestroy()
+    {
+        ContainerHolder.Container.Unregister<MenuOpener>();
     }
 }
--- Assets/Scripts/Game/GameManagement/GameManagementService.cs
+++ Assets/Scripts/Game/GameManagement/GameManagementService.cs
@@ -1,21 +1,33 @@
 ﻿using Core.Services;
 using Core.Services.SceneManagement;
 using Game.Items;
+using Tools.Unity;
 using UnityDI;
+using UnityEngine;
 
 namespace Game.GameManagement
 {
     public class GameManagementService : ILoadableService, IUnloadableService
     {
+        [Dependency]
+        private UnityEventProvider unityEventProvider;
         [Dependency] 
         private readonly SceneManagerService _SceneManagerService;
         [Dependency] 
         private readonly ItemsService _ItemsService;
-        
-        
+        [Dependency]
+        private MenuOpener menu
+        {
+            get
+            {
+                return ContainerHolder.Container.Resolve<MenuOpener>();
+            }
+        }
+
         public void Load()
         {
-            
+            unityEventProvider.OnUpdate += CheckForPauseButton;
+            Debug.Log("here");
         }
 
         public void Unload()
@@ -27,6 +39,14 @@ namespace Game.GameManagement
         {
             _ItemsService.ClearAllItems();
             _SceneManagerService.LoadScene(SceneType.GameLevel_1, true);
+        }
+
+        public void CheckForPauseButton()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                menu.Pause();
+            }
         }
     }
 }
